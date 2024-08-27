0ccf215d914b5bab5acc021bf3eb1f6ce0727266
Deshaun Robinson <deshaun.robinson@outlook.com>
2024-08-27 17:30:44 -0400

    changed sizing and added input-container componenet

--- protocall/src/app/home/home.component.ts
+++ protocall/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InputContainerComponent } from '../input-container/input-container.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [InputContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
