c0868163666c64ae2084cc6ce815d2ff930d8502
Deshaun Robinson <deshaun.robinson@outlook.com>
2024-08-29 10:35:35 -0400

    general design change and added output component

---
+++ protocall/src/app/output-container/output-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './output-container.component.html',
+  styleUrl: './output-container.component.css'
+})
+export class OutputContainerComponent {
+
+}
